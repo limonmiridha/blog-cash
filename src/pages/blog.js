27163d3f10b2281193db79d22cb74452f35651a6
limonmiridha <limonmiridha100@gmail.com>
2023-08-06 10:30:06 +0600

    blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,288 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <div className="container mx-auto px-4 md:px-8 py-8">
+      {/* <h2 className="text-4xl font-bold my-4">Blog.</h2> */}
+
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/cover.webp" fill alt="cover" />
+        </div>
+        <h2 className="text-2xl font-bold my-4">Sell With Us</h2>
+        <p className="my-4">
+          Utilizing unprecedented resources, we will craft a unique and winning
+          strategy to sell your real estate. Let us put our experience to work
+          for you.
+        </p>
+        <div className="my-4">
+          <p>Ready to Sell? Let's Connect. </p>
+          <p>Call & Text 612-400-9000</p>
+          <p>M@1MW.com</p>
+        </div>
+      </div>
+      {/* 2nd row */}
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/blog-1.jpg" fill alt="cover" />
+        </div>
+
+        <p className="my-4">
+          Wilen is made up of 10 highly experienced, multidisciplinary
+          professionals who apply their high-level real estate knowledge to a
+          wide range of transactions. The Group has methodically built and
+          developed a powerhouse real estate practice that sits prominently
+          within the market. Because Wilen understands each individual selling
+          situation is different, it has structured its real estate practice to
+          meet diverse needs. The team’s knowledge knows no bounds and includes
+          major asset classes such as single family residential, condos,
+          multifamily, duplex/triplex, student housing, commercial, apartments,
+          leisure, development, condo-conversion, land, acute medical, retail,
+          industrial and foreclosure (REO).
+        </p>
+      </div>
+      {/* 3rd Row */}
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/blog-2.jpg" fill alt="cover" />
+        </div>
+        <div>
+          <p className="my-4">Mike Wilen</p>
+          <p className="my-4">Real Estate Agent</p>
+          <p className="my-4">
+            Find a challenge. Create a good, cost-effective solution. Repeat.
+          </p>
+          <p className="my-4">612-400-9000</p>
+          <p className="my-4">Mike@1MW.com</p>
+          <p className="my-4">Co-Founder Wilen</p>
+          <p className="my-4">700+ Properties Sold as Listing Agent</p>
+          <p className="my-4">Presidents Club</p>
+          <p className="my-4">Global Luxury Team</p>
+          <p className="my-4">
+            Assisting 30+ of the world’s largest lenders in the sale and
+            valuation of real estate.
+          </p>
+          <p className="my-4">
+            Developed a loss severity percentage tool with proprietary intrinsic
+            value estimators, helping lenders predict future market challenges.
+          </p>
+          <p className="my-4">
+            300+ lender owned properties sold as listing agent, 1,000+
+            properties managed through eviction and over 10,000+ portfolio
+            valuations.
+          </p>
+          <p className="my-4">
+            Information agitator for crucial real estate insights, benefiting
+            investors and homeowners alike.
+          </p>
+          <p className="my-4">
+            Sales & Development: 30+ unit conversion development in Minneapolis.
+            Sales & Development: 54+ unit development in Eden Prairie.
+          </p>
+          <p className="my-4">
+            Sales & Development: 20+ unit development in Delano (full sellout).
+          </p>
+          <p className="my-4">
+            Sales & Development: 54+ unit development in Eden Prairie.
+          </p>
+          <p className="my-4">
+            Sales & Development: 20+ unit development in Delano (full sellout).
+          </p>
+          <p className="my-4">
+            Land Sales: 100-acre land configuration in Credit River Township,
+            leading to a successful sale.
+          </p>
+          <p className="my-4">
+            Sales & Development: 130-unit condo conversion and development
+            project in Minnetonka.
+          </p>
+          <p className="my-4">
+            Municipal Facilitation: County and City easement agreements for a
+            large-scale development project, Monticello.
+          </p>
+          <p className="my-4">
+            Planning: Local and national utility agreements and easement
+            negotiations; 800+ residents. Planning & Facilitation: Comprehensive
+            configuration and plan, allowing for a $10M refinance in Zumbrota.
+          </p>
+          <p className="my-4">
+            Frequently Mentioned Across Various News Organizations and
+            Publications: Fox News Super Bowl Rentals; Minneapolis Saint Paul
+            Business: $2.6 Million Lake Minnetonka Commercial Space: Star
+            Tribune Home Sales; New York Times Luxury Home; USA Today
+            Mississippi River Bluff New Construction; Wall Street Journal Upper
+            Bracket Listings; Twin Cities Business Super-Agent; USA Today Bluff
+            Residence; St. Cloud Times Victorian; Star Tribune Queen Anne
+            Residence; ICANN Review and A+ Domain Name Holder; Acquisition and
+            Facilitation of High-Level Domain Names, Both Personal and
+            Commercial.
+          </p>
+        </div>
+      </div>
+      {/* 4th row */}
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/cover.webp" fill alt="cover" />
+        </div>
+
+        <p className="my-4">
+          To date, Wilen has assisted 35+ of the largest and most sophisticated
+          local, global, public, and private lenders in the sale and valuation
+          of foreclosure real estate. With over 300+ sales handled as a listing
+          agent, 1,000+ properties managed through eviction proceedings, and
+          more than 10,000+ portfolio valuations conducted, Wilen has an
+          extensive track record of success. The REO Group consists of two
+          tenacious field professionals who flawlessly execute assigned REO
+          tasks, completing property occupancy checks, re-keys, cash for keys
+          transactions, property postings, utility hookup, and rehab bid
+          coordination within 6 hours of assignment. Moreover, Wilen provides
+          unmatched syndication and attracts a lion's share of interest with
+          their proprietary foreclosure-related websites:
+          MinnesotaForeclosures.com, SaintPaulForeclosures.com,
+          MinneapolisForeclosures.com, and NONMLS.com.
+        </p>
+      </div>
+      {/* 5th row */}
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/blog-1.jpg" fill alt="cover" />
+        </div>
+
+        <p className="my-4">
+          Successful marketing strategies are crucial for showcasing properties
+          in their best light and attracting potential buyers. We have carefully
+          curated ideas and implementations that significantly impact the real
+          estate buyer's journey:
+        </p>
+        <p className="my-4">
+          Foster meaningful connections at the property(s) by understanding
+          potential buyers' needs and preferences.
+        </p>
+        <p className="my-4">
+          Showcase your property's best features through high-quality
+          photography, engaging virtual tours, and attractive and spontaneous
+          write-ups that vividly portray its unique qualities, igniting
+          potential buyers' imagination.
+        </p>
+        <p className="my-4">
+          Introduce potential buyers to the property's surroundings and
+          neighborhood, creating a sense of community and belonging.
+        </p>
+        <p className="my-4">
+          Introduce potential buyers to the property's surroundings and
+          neighborhood, creating a sense of community and belonging.
+        </p>
+        <p className="my-4">
+          Utilize eye-catching For Sale signs as an effective touchpoint to
+          attract local buyers and generate interest in your property.
+        </p>
+        <p className="my-4">
+          Leverage staging expertise to present your property at its best,
+          allowing potential buyers to envision themselves living in their dream
+          home.
+        </p>
+        <p className="my-4">
+          Engage in digital outreach efforts targeting a vast local and national
+          audience, maximizing visibility through popular print and digital
+          touchpoints.
+        </p>
+        <p className="my-4">
+          Create customized, illustrated digital, and print marketing materials,
+          including professional photography, floor plans, and key selling
+          features.
+        </p>
+        <p className="my-4">
+          Co-broke your listing with other members of the brokerage community to
+          maximize exposure to qualified buyers and access a network of
+          individuals actively searching for prime real estate opportunities.
+        </p>
+        <p className="my-4">
+          Two Certified Relocation Specialists with extensive expertise and
+          in-depth knowledge of local regions - with a proven track record of
+          successfully introducing new residents to Minnesota's neighborhoods.
+        </p>
+        <p className="my-4">
+          Skilled web designers and developers who create captivating,
+          user-friendly websites to showcase property nuances in the best
+          possible light. Thereafter our team implements a serious of search
+          engine optimization (SEO) techniques to ensure your property ranks
+          highly in search results, attracting a steady stream of interested
+          prospects.
+        </p>
+        <p className="my-4">
+          Counseling borrowers in a wide variety of real estate financing
+          matters across the entire capital structure including, conventional,
+          FHA, USDA, corporate term loans, revolving credit, rehab, mezzanine,
+          short-term bridge financing, credit lines, and balloon payment CD’s.
+        </p>
+        <p className="my-4">
+          Full knowledge of all activity associated with your property.
+        </p>
+      </div>
+      {/* 6th row */}
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/blog-1.jpg" fill alt="cover" />
+        </div>
+
+        <p className="my-4">
+          In today's digital era, Real Estate lives online, captivating
+          consumers at various touchpoints and offering valuable insights and
+          experiences during the home-buying journey. Pioneering this dynamic
+          landscape, Wilen has developed a single-entry, no-code content hub to
+          personalize and recommend real estate to consumers across diverse
+          channels. This innovative approach ensures a precise and compelling
+          presentation of your property's distinct value while dispelling any
+          misconceptions along the way. This is a glimmer into several Wilen
+          touchpoints:
+        </p>
+        <p className="my-4">
+          LakeMinnetonkaHouses.com 1mw.com NONMLS.com MikeWilen.com
+          LakeMinnetonkaRentals.com LakeMinnetonkaGroup.com
+          LakeMinnetonkaTownHomes.com ShakopeeHouses.com ILRET.com
+          LakevilleHouses.com MaplePlainRealEstate.com MinneapolisCondoGroup.com
+          MinneapolisForeclosures.com MinneapolisNorthloop.com
+          MinnesotaForeclosures.com MinnesotaHomesRealEstate.com
+          MinnetristaHomes.com MNHomesRealEstate.com MNRealEstateHomes.com
+          RichfieldTownHomes.com SaintAnthonyHomes.com SaintLouisParkHouses.com
+          SaintPaulForeclosures.com SpringLakeParkHomes.com
+          SpringLakeParkRealEstate.com WilenRealEstate.com BlaineCondos.com
+          BloomingtonTownHomes.com BurnsvilleCondos.com BurnsvilleHouses.com
+          BurnsvilleTownHomes.com ChicagoRealtyCapital.com DeephavenHomes.com
+          EaganCondos.com GoldenValleyTownHomes.com WilenHomes.com
+          WilenRealty.codm
+        </p>
+      </div>
+      {/* 7th row */}
+      <div>
+        <div className="w-full h-[50vw] md:h-[500px] relative">
+          <Image src="/cover.webp" fill alt="cover" />
+        </div>
+
+        <p className="my-4">
+          LakeMinnetonkaHouses.com 1mw.com NONMLS.com MikeWilen.com
+          LakeMinnetonkaRentals.com LakeMinnetonkaGroup.com
+          LakeMinnetonkaTownHomes.com ShakopeeHouses.com ILRET.com
+          LakevilleHouses.com MaplePlainRealEstate.com MinneapolisCondoGroup.com
+          MinneapolisForeclosures.com MinneapolisNorthloop.com
+          MinnesotaForeclosures.com MinnesotaHomesRealEstate.com
+          MinnetristaHomes.com MNHomesRealEstate.com MNRealEstateHomes.com
+          RichfieldTownHomes.com SaintAnthonyHomes.com SaintLouisParkHouses.com
+          SaintPaulForeclosures.com SpringLakeParkHomes.com
+          SpringLakeParkRealEstate.com WilenRealEstate.com BlaineCondos.com
+          BloomingtonTownHomes.com BurnsvilleCondos.com BurnsvilleHouses.com
+          BurnsvilleTownHomes.com ChicagoRealtyCapital.com DeephavenHomes.com
+          EaganCondos.com GoldenValleyTownHomes.com WilenHomes.com
+          WilenRealty.codm
+        </p>
+        <p className="my-4">We hope you find this information useful.</p>
+        <p className="my-4">Let’s Talk & Text</p>
+        <p className="my-4">612-400-9000</p>
+        <p className="my-4">M@1MW.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
